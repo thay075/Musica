11156534b506a6557e0b5be8f95fc1eb10ad4dfc
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 20:02:12 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.InteropServices;
+
+Console.BackgroundColor =ConsoleColor.DarkBlue;
+Console.ForegroundColor =ConsoleColor.Magenta;
+Console.Write("Softcore- The Neighbourhood");
+Console.ResetColor();
+
+
+
+
+Console.WriteLine(@"
+
+You've been my muse for a long time
+You get me through every dark night
+I'm always gone, out on the go
+I'm on the run and you're home alone
+I'm too consumed with my own life
+
+Are we too young for this?
+Feels like I can't move
+");
+
+Console.BackgroundColor =ConsoleColor.DarkRed;
+Console.ForegroundColor =ConsoleColor.DarkYellow;
+Console.Write(@"Sharing my heart
+It's tearing me apart
+But I know I'd miss you, baby, if I left right now
+Doing what I can, tryna be a man
+And every time I kiss you, baby
+I can hear the sound of breaking down");
+Console.ResetColor();
+
+Console.WriteLine(@"
+I've been confused as of late (yeah)
+Watching my youth slip away (yeah)
+You're like the Sun, you wake me up
+But you drain me out if I get too much
+I might need you or I'll break
+
+Are we too young for this?
+Feels like I can't move
+");
+
+Console.BackgroundColor =ConsoleColor.DarkRed;
+Console.ForegroundColor =ConsoleColor.DarkYellow;
+Console.Write(@"Sharing my heart
+It's tearing me apart
+But I know I'd miss you, baby, if I left right now
+Doing what I can, tryna be a man
+And every time I kiss you, baby
+I can hear the sound of breaking down");
+Console.ResetColor();
+
+Console.WriteLine(@"
+(Breaking down, breaking down, breaking down)
+(Breaking down, breaking down, breaking down)
+
+I don't want to play this part
+But I do, all for you
+I don't want to make this hard
+But I will 'cause I'm still
+");
+
+Console.BackgroundColor =ConsoleColor.DarkRed;
+Console.ForegroundColor =ConsoleColor.DarkYellow;
+Console.Write(@"Sharing my heart
+It's tearing me apart
+But I know I'd miss you, baby, if I left right now
+Doing what I can, tryna be a man
+And every time I kiss you, baby
+I can hear the sound of breaking down");
+Console.ResetColor();
+
+Console.WriteLine(@"
+Sharing my bed, ooh
+Sharing my bread, yeah
+Sharing my bread
+Sharing my head
+(I'm breaking down)
+
+Sharing my heart
+Sharing my, suddenly I'm
+(Breaking down)
+Sharing, I'm done
+Sharing my life");
